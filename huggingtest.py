6143a5b421aebf48aebeab305a6d3d06314577c7
khushboogupta24 <kgupta5@scu.edu>
2024-04-03 15:14:56 -0700

    First commit

---
+++ huggingtest.py
@@ -0,0 +1,13 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+from PIL import Image
+
+pipe = pipeline("object-detection", model="valentinafeve/yolos-fashionpedia")
+
+image_path = "dancingqueen.jpeg"
+image = Image.open(image_path)
+
+predictions = pipe(image)
+
+for prediction in predictions:
+    print(f"Label: {prediction['label']}, Score: {prediction['score']}, Box: {prediction['box']}")
